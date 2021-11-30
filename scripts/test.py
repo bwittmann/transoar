e978fd95797e281f579ba1769d3578532fb5dab9
bastian <bastian.wittmann@tum.de>
2021-11-30 12:32:13 +0100

    Refactoring: Included prev functions into Tester class.

--- scripts/test.py
+++ scripts/test.py
@@ -12,67 +12,70 @@ from transoar.data.dataloader import get_loader
 from transoar.models.transoarnet import TransoarNet
 from transoar.evaluator import DetectionEvaluator
 
-def test(args):
-    path_to_run = Path(args.run)
-    config = load_json(path_to_run / 'config.json')
-    device = 'cuda:' + str(args.num_gpu)
+class Tester:
 
-    # Get path to checkpoint
-    avail_checkpoints = [path for path in path_to_run.iterdir() if 'model_' in str(path)]
-    avail_checkpoints.sort(key=lambda x: len(str(x)))
-    if args.last:
-        path_to_ckpt = avail_checkpoints[0]
-    else:
-        path_to_ckpt = avail_checkpoints[-1]
+    def __init__(self, args):
+        path_to_run = Path(args.run)
+        config = load_json(path_to_run / 'config.json')
+        self._device = 'cuda:' + str(args.num_gpu)
 
-    # Create dir to store results
-    path_to_results = path_to_run / 'results' / path_to_ckpt.parts[-1][:-3]
-    try:
-        path_to_results.mkdir(parents=True, exist_ok=False)
-    except: 
-        pass
+        # Get path to checkpoint
+        avail_checkpoints = [path for path in path_to_run.iterdir() if 'model_' in str(path)]
+        avail_checkpoints.sort(key=lambda x: len(str(x)))
+        if args.last:
+            path_to_ckpt = avail_checkpoints[0]
+        else:
+            path_to_ckpt = avail_checkpoints[-1]
 
-    # Build necessary components
-    set_to_eval = 'val' if args.val else 'test'
-    test_loader = get_loader(config['data'], set_to_eval)
+        # Create dir to store results
+        self._path_to_results = path_to_run / 'results' / path_to_ckpt.parts[-1][:-3]
+        try:
+            self._path_to_results.mkdir(parents=True, exist_ok=False)
+        except: 
+            pass
 
-    evaluator = DetectionEvaluator(classes=list(config['data']['labels'].values()))
-    model = TransoarNet(config['model'], config['data']['num_classes']).to(device=device)
+        # Build necessary components
+        self._set_to_eval = 'val' if args.val else 'test'
+        self._test_loader = get_loader(config['data'], self._set_to_eval)
 
-    # Load checkpoint
-    checkpoint = torch.load(path_to_ckpt)
-    model.load_state_dict(checkpoint['model_state_dict'])
-    model.eval()
+        self._evaluator = DetectionEvaluator(classes=list(config['data']['labels'].values()))
+        self._model = TransoarNet(config['model'], config['data']['num_classes']).to(device=self._device)
 
-    with torch.no_grad():
-        for data, mask, bboxes, _ in tqdm(test_loader):
-            # Put data to gpu
-            data, mask = data.to(device=device), mask.to(device=device)
-        
-            targets = []
-            for item in bboxes:
-                target = {
-                    'boxes': item[0].to(dtype=torch.float, device=device),
-                    'labels': item[1].to(device=device)
-                }
-                targets.append(target)
+        # Load checkpoint
+        checkpoint = torch.load(path_to_ckpt)
+        self._model.load_state_dict(checkpoint['model_state_dict'])
+        self._model.eval()
 
-            # Make prediction
-            out = model(data, mask)
+    def run(self):
+        with torch.no_grad():
+            for data, mask, bboxes, _ in tqdm(self._test_loader):
+                # Put data to gpu
+                data, mask = data.to(device=self._device), mask.to(device=self._device)
+            
+                targets = []
+                for item in bboxes:
+                    target = {
+                        'boxes': item[0].to(dtype=torch.float, device=self._device),
+                        'labels': item[1].to(device=self._device)
+                    }
+                    targets.append(target)
 
-            # Add pred to evaluator
-            pred_probs = F.softmax(out['pred_logits'], dim=-1)
-            evaluator.add(
-                pred_boxes=[boxes.detach().cpu().numpy() for boxes in out['pred_boxes']],
-                pred_classes=[torch.max(probs, dim=-1)[1].detach().cpu().numpy() for probs in pred_probs],
-                pred_scores=[torch.max(probs, dim=-1)[0].detach().cpu().numpy() for probs in pred_probs],
-                gt_boxes=[target['boxes'].detach().cpu().numpy() for target in targets],
-                gt_classes=[target['labels'].detach().cpu().numpy() for target in targets]
-            )
+                # Make prediction
+                out = self._model(data, mask)
 
-        # Get and store final results
-        metric_scores = evaluator.eval()
-        write_json(metric_scores, path_to_results / ('results_' + set_to_eval))
+                # Add pred to evaluator
+                pred_probs = F.softmax(out['pred_logits'], dim=-1)
+                self._evaluator.add(
+                    pred_boxes=[boxes.detach().cpu().numpy() for boxes in out['pred_boxes']],
+                    pred_classes=[torch.max(probs, dim=-1)[1].detach().cpu().numpy() for probs in pred_probs],
+                    pred_scores=[torch.max(probs, dim=-1)[0].detach().cpu().numpy() for probs in pred_probs],
+                    gt_boxes=[target['boxes'].detach().cpu().numpy() for target in targets],
+                    gt_classes=[target['labels'].detach().cpu().numpy() for target in targets]
+                )
+
+            # Get and store final results
+            metric_scores = self._evaluator.eval()
+            write_json(metric_scores, self._path_to_results / ('results_' + self._set_to_eval))
 
 
 if __name__ == "__main__":
@@ -80,10 +83,11 @@ if __name__ == "__main__":
 
     # Add necessary args
     parser.add_argument('--run', required=True, type=str, help='Path to experiment in transoar/runs.')
-
     parser.add_argument('--num_gpu', type=int, default=0, help='Use model_last instead of model_best.')
     parser.add_argument('--val', action='store_true', help='Evaluate performance on test set.')
     parser.add_argument('--last', action='store_true', help='Use model_last instead of model_best.')
-
+    parser.add_argument('--save_preds', action='store_true', help='Save predictions.')
     args = parser.parse_args()
-    test(args)
+
+    tester = Tester(args)
+    tester.run()
