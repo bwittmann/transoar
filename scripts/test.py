91853408c3a6bf43a563b9e2f9d578f82c109bf2
bastian <bastian.wittmann@tum.de>
2022-07-04 20:06:53 +0200

    Added bootstrapping

--- scripts/test.py
+++ scripts/test.py
@@ -1,9 +1,11 @@
 """Script to evalute performance on the val and test set."""
 
+import random
 import argparse
 from collections import defaultdict
 from pathlib import Path
 
+import monai
 import torch
 import numpy as np
 from tqdm import tqdm
@@ -36,6 +38,12 @@ class Tester:
 
         # Build necessary components
         self._set_to_eval = 'val' if args.val else 'test'
+
+        torch.manual_seed(config['seed'])
+        np.random.seed(config['seed'])
+        monai.utils.set_determinism(seed=config['seed'])
+        random.seed(config['seed'])
+
         self._test_loader = get_loader(config, self._set_to_eval, batch_size=1)
 
         self._evaluator = DetectionEvaluator(
@@ -64,57 +72,67 @@ class Tester:
   
     def run(self):
         with torch.no_grad():
-            for idx, (data, _, bboxes, seg_mask) in enumerate(tqdm(self._test_loader)):
-                # Put data to gpu
-                data = data.to(device=self._device)
-            
-                targets = defaultdict(list)
-                for item in bboxes:
-                    targets['target_boxes'].append(item[0].to(dtype=torch.float, device=self._device))
-                    targets['target_classes'].append(item[1].to(device=self._device))
-                targets['target_seg'] = seg_mask.squeeze(1).to(device=self._device)
+            scores = []
+            for _ in range(25):
+                passed = 0
+                for idx, (data, _, bboxes, seg_mask) in enumerate(self._test_loader):
+                    # Put data to gpu
+                    data = data.to(device=self._device)
 
-                # Only use complete data for performance evaluation
-                if self._full_labeled:
-                    if targets['target_classes'][0].shape[0] < len(self._class_dict):
-                        continue
+                    targets = defaultdict(list)
+                    for item in bboxes:
+                        targets['target_boxes'].append(item[0].to(dtype=torch.float, device=self._device))
+                        targets['target_classes'].append(item[1].to(device=self._device))
+                    targets['target_seg'] = seg_mask.squeeze(1).to(device=self._device)
 
-                # Make prediction
-                _, predictions = self._model.train_step(data, targets, evaluation=True)
+                    # Only use complete data for performance evaluation
+                    if self._full_labeled:
+                        if targets['target_classes'][0].shape[0] < len(self._class_dict):
+                            continue
 
-                pred_boxes = predictions['pred_boxes'][0].detach().cpu().numpy()
-                pred_classes = predictions['pred_labels'][0].detach().cpu().numpy()
-                pred_scores = predictions['pred_scores'][0].detach().cpu().numpy()
-                gt_boxes = targets['target_boxes'][0].detach().cpu().numpy()
-                gt_classes = targets['target_classes'][0].detach().cpu().numpy()
+                    passed += 1
+                    # print(passed)
 
-                # Evaluate validation predictions based on metric
-                self._evaluator.add(
-                    pred_boxes=[pred_boxes],
-                    pred_classes=[pred_classes],
-                    pred_scores=[pred_scores],
-                    gt_boxes=[gt_boxes],
-                    gt_classes=[gt_classes],
-                )
+                    # Make prediction
+                    _, predictions = self._model.train_step(data, targets, evaluation=True)
 
-                # Just take most confident prediction per class
-                best_ids = []
-                for class_ in np.unique(pred_classes):
-                    class_ids = (pred_classes == class_).nonzero()[0]
-                    max_scoring_idx = pred_scores[class_ids].argmax()
-                    best_ids.append(class_ids[max_scoring_idx])
-                best_ids = torch.tensor(best_ids)
+                    pred_boxes = predictions['pred_boxes'][0].detach().cpu().numpy()
+                    pred_classes = predictions['pred_labels'][0].detach().cpu().numpy()
+                    pred_scores = predictions['pred_scores'][0].detach().cpu().numpy()
+                    gt_boxes = targets['target_boxes'][0].detach().cpu().numpy()
+                    gt_classes = targets['target_classes'][0].detach().cpu().numpy()
 
-                if self._save_preds:
-                    save_pred_visualization(
-                        box_xyxyzz_to_cxcyczwhd(pred_boxes[best_ids], data.shape[2:]), pred_classes[best_ids] + 1, 
-                        box_xyxyzz_to_cxcyczwhd(gt_boxes, data.shape[2:]), gt_classes + 1, 
-                        seg_mask[0], self._path_to_vis, self._class_dict, idx
+                    # Evaluate validation predictions based on metric
+                    self._evaluator.add(
+                        pred_boxes=[pred_boxes],
+                        pred_classes=[pred_classes],
+                        pred_scores=[pred_scores],
+                        gt_boxes=[gt_boxes],
+                        gt_classes=[gt_classes],
                     )
 
-            # Get and store final results
-            metric_scores = self._evaluator.eval()
-            write_json(metric_scores, self._path_to_results / ('results_' + self._set_to_eval))
+                    # Just take most confident prediction per class
+                    best_ids = []
+                    for class_ in np.unique(pred_classes):
+                        class_ids = (pred_classes == class_).nonzero()[0]
+                        max_scoring_idx = pred_scores[class_ids].argmax()
+                        best_ids.append(class_ids[max_scoring_idx])
+                    best_ids = torch.tensor(best_ids)
+
+                    if self._save_preds:
+                        save_pred_visualization(
+                            box_xyxyzz_to_cxcyczwhd(pred_boxes[best_ids], data.shape[2:]), pred_classes[best_ids] + 1, 
+                            box_xyxyzz_to_cxcyczwhd(gt_boxes, data.shape[2:]), gt_classes + 1, 
+                            seg_mask[0], self._path_to_vis, self._class_dict, idx
+                        )
+                    if passed == 4:
+                        break
+
+                # Get and store final results
+                metric_scores = self._evaluator.eval()
+                scores.append(metric_scores['mAP_coco'])
+                # write_json(metric_scores, self._path_to_results / ('results_' + self._set_to_eval))
+            k = 12
 
 
 if __name__ == "__main__":
